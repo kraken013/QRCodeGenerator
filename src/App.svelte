308e082d10952795a7bbf8da8b0b50f968edcedd
kraken013 <sofiane.flici14@gmail.com>
2023-11-07 22:36:11 +0100

    start project

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import NavBar from './components/NavBar.svelte';
+</script>
+
+<NavBar />
+<main>
+  <h1>QR Code Generator</h1>
+  <p>
+    This is a QR Code Generator built with Svelte. It uses the 
+  </p>
+</main>
---
+++ src/components/NavBar.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+    import svelteLogo from './../assets/svelte.svg';
+    import Switch from './../lib/Switch.svelte';
+    let language: string = 'fr';
+    let theme: string = 'light';
+
+    const changeTheme = () => {
+        if (theme === 'light') {
+            theme = 'dark';
+        } else {
+            theme = 'light';
+        }
+    }
+
+    const changeLanguage = () => {
+        if (language === 'fr') {
+            language = 'en';
+        } else {
+            language = 'fr';
+        }
+    }
+</script>
+
+<nav>
+    <ul>
+        <li><img src={svelteLogo} alt="Svelte Logo" /></li>
+        <li><h1>QR Code Generator</h1></li>        
+    </ul>
+
+    <ul>
+        <li>
+            <Switch bind:value={theme} label="" options={['light', 'dark']} fontSize={12} on:click={changeTheme} />
+        </li>
+        <li>
+            <button on:click={changeLanguage}>
+                {#if language === 'fr'}
+                  <span>FR</span>
+                {:else}
+                  <span>EN</span>
+                {/if}
+            </button>
+        </li>
+    </ul>
+</nav>
